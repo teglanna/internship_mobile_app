91e882742915dff6d5d6d4340253a1ace3a9e8a5
ejulcsi <ejulcsi@gmail.com>
2015-08-19 15:03:31 +0200

    settings.py db configured

--- backend/swipebuy/swipebuy/settings.py
+++ backend/swipebuy/swipebuy/settings.py
@@ -81,8 +81,12 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 
-        'ENGINE': 'django.contrib.gis.db.backends.spatialite',
-        'NAME': 'geodjango',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'pulilab',
+        'HOST': '192.168.99.100',
+        'PORT': '5432',
 
     }
 }
