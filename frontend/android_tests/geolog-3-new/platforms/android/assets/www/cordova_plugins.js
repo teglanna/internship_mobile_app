27c848622ae504e654abd62199a3d31aec589e3e
teglanna <teglanna@gmail.com>
2015-08-24 18:26:11 +0200

    Geo3 added

---
+++ frontend/android_tests/geolog-3-new/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,24 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova.plugins.diagnostic/www/diagnostic.js",
+        "id": "cordova.plugins.diagnostic.Diagnostic",
+        "clobbers": [
+            "cordova.plugins.diagnostic"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.0.0",
+    "cordova-plugin-geolocation": "1.0.2-dev",
+    "cordova.plugins.diagnostic": "0.0.1"
+}
+// BOTTOM OF METADATA
+});
---
+++ frontend/android_tests/geolog-3-new/platforms/android/bin/classes.dex.d
@@ -0,0 +1,13 @@
+/home/pulilab/geolocation/platforms/android/bin/classes.dex : \
+/home/pulilab/geolocation/platforms/android/bin/classes/cordova/plugins/Diagnostic.class \
+/home/pulilab/geolocation/platforms/android/bin/classes/com/example/hello/MainActivity.class \
+/home/pulilab/geolocation/platforms/android/bin/classes/com/example/hello/R$drawable.class \
+/home/pulilab/geolocation/platforms/android/bin/classes/com/example/hello/BuildConfig.class \
+/home/pulilab/geolocation/platforms/android/bin/classes/com/example/hello/R$attr.class \
+/home/pulilab/geolocation/platforms/android/bin/classes/com/example/hello/R$string.class \
+/home/pulilab/geolocation/platforms/android/bin/classes/com/example/hello/R$xml.class \
+/home/pulilab/geolocation/platforms/android/bin/classes/com/example/hello/R.class \
+/home/pulilab/geolocation/platforms/android/bin/classes/org/apache/cordova/whitelist/WhitelistPlugin$CustomConfigXmlParser.class \
+/home/pulilab/geolocation/platforms/android/bin/classes/org/apache/cordova/whitelist/WhitelistPlugin$1.class \
+/home/pulilab/geolocation/platforms/android/bin/classes/org/apache/cordova/whitelist/WhitelistPlugin.class \
+/home/pulilab/geolocation/platforms/android/bin/dexedLibs/classes-0590e78778d3ac60984be210c10eab48.jar \
