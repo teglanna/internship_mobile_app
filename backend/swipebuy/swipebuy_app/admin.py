4ce8e9babdf2eccafff71470ec8053af47f85926
Fekete Gyorgy <f@pulilab.com>
2015-09-03 14:52:16 +0200

    register admins

--- backend/swipebuy/swipebuy_app/admin.py
+++ backend/swipebuy/swipebuy_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Stuff, Message, SwipeAction, Image, CheckIn
 
-# Register your models here.
+
+admin.site.register(Stuff)
+admin.site.register(Message)
+admin.site.register(SwipeAction)
+admin.site.register(Image)
+admin.site.register(CheckIn)
