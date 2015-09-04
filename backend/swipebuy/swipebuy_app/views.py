ba9378fccc2e0a636c3a38399f766023c97122d9
Fekete Gyorgy <f@pulilab.com>
2015-09-04 17:23:12 +0200

    modelview sets with custom get_object(s)

--- backend/swipebuy/swipebuy_app/views.py
+++ backend/swipebuy/swipebuy_app/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 from .serializers import MessageSerializer, StuffSerializer, ImageSerializer, ActionSerializer
 from .models import Message, Stuff, Image, SwipeAction
 
@@ -9,14 +10,41 @@ class MessageViewSet(viewsets.ModelViewSet):
     """
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
-    lookup_field = 'pk'
+    lookup_field = 'stuff_id'
+
+    def get_object(self):
+
+        queryset = self.filter_queryset(self.get_queryset())
+
+        # Perform the lookup filtering.
+        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
+
+        assert lookup_url_kwarg in self.kwargs, (
+            'Expected view %s to be called with a URL keyword argument '
+            'named "%s". Fix your URL conf, or set the `.lookup_field` '
+            'attribute on the view correctly.' %
+            (self.__class__.__name__, lookup_url_kwarg)
+        )
+
+        filter_kwargs = {self.lookup_field: self.kwargs[lookup_url_kwarg]}
+        obj = queryset.filter(**filter_kwargs)
+
+        # May raise a permission denied
+        self.check_object_permissions(self.request, obj)
+
+        return obj
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, many=True)
+        return Response(serializer.data)
 
 
 class StuffViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
-    queryset = Stuff.objects.all()
+    queryset = Stuff.objects.sort_by_distance()
     serializer_class = StuffSerializer
     lookup_field = 'pk'
 
